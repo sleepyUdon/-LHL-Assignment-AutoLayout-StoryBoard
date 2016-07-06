7cfede6e3e297a95ebb2058d863c7a0637db3a0f
sleepyUdon <vivianechan@hotmail.com>
2016-07-05 21:07:52 -0400

    first commit

---
+++ Autolayout - Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Autolayout - Storyboard
+//
+//  Created by Viviane Chan on 2016-07-05.
+//  Copyright © 2016 LightHouse Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
